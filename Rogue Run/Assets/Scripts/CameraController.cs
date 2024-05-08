95c238bd48d45ce1f1ac923d863fdb86723872c2
Nano <86750728+Nano-233@users.noreply.github.com>
2024-05-08 10:44:06 +0800

    Room panning and follow camera
    Follow camera has no lookahead.

--- Rogue Run/Assets/Scripts/CameraController.cs
+++ Rogue Run/Assets/Scripts/CameraController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
+    //Room Camera
     [SerializeField] private float speed; //speed of camera
     private float _currentPosX;
     private Vector3 _velocity = Vector3.zero;
+    
+    //Player camera
+    [SerializeField] private Transform player;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position,
-            new Vector3(_currentPosX, transform.position.y, transform.position.z),
-            ref _velocity, speed);
+        //room camera
+        // transform.position = Vector3.SmoothDamp(transform.position,
+        //     new Vector3(_currentPosX, transform.position.y, transform.position.z),
+        //     ref _velocity, speed);
+        
+        //follow player
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 
     //Moves to new room.
