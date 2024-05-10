4e896e6cda5b7534e9cd79f6de9a4643b46bc208
Nano <86750728+Nano-233@users.noreply.github.com>
2024-05-10 15:15:59 +0800

    Fixed camera panning between rooms

--- Rogue Run/Assets/Scripts/Door.cs
+++ Rogue Run/Assets/Scripts/Door.cs
@@ -2,27 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using UnityEditor.Rendering.BuiltIn.ShaderGraph;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] private CinemachineConfiner confiner;
-    [SerializeField] private Collider2D nextRoom;
     [SerializeField] private Collider2D previousRoom;
+    [SerializeField] private Collider2D nextRoom;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         //if colliding with a player。
         if (collision.CompareTag("Player"))
         {
-            //moves camera to room depending on position coming from.
+            /*moves camera to room depending on position coming from.
+             Because it's OnTriggerExit, the player x value will be larger than trigger if going right*/
             if (collision.transform.position.x < transform.position.x)
             {
-                confiner.m_BoundingShape2D = nextRoom;
+                confiner.m_BoundingShape2D = previousRoom;
             }
             else
             {
-                confiner.m_BoundingShape2D = previousRoom;
+                confiner.m_BoundingShape2D = nextRoom;
             }
         }
     }
