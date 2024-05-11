ee65d1e1b2c4262c4d07ac7f3c808d31b94dd142
meerkatboy <sohjunanerwin@gmail.com>
2024-05-11 11:22:00 +0800

    added everything back in
    added them back in

--- Rogue Run/Assets/Scripts/PlayerController.cs
+++ Rogue Run/Assets/Scripts/PlayerController.cs
@@ -13,13 +13,49 @@ public class PlayerController : MonoBehaviour
     
     private Vector2 _moveInput; //gets vector from player input.
     private Rigidbody2D _rb; //Rigidbody2D of player.
-    
-    public bool IsMoving { get; private set; }
+    private Animator _animator; //animator of the player sprite
+
+    private bool _isMoving = false; //is the player moving
+    public bool isFacingRight = true; //which way the player is facing
+
+    //checks if the player is moving
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            //sync with the animator
+            _animator.SetBool(AnimationStrings.IsMoving, value);
+        }
+    }
+
+    public bool IsFacingRight
+    {
+        get
+        {
+            return isFacingRight;
+        } private set
+        {
+            //if facing is wrong
+            if (isFacingRight != value)
+            {
+                //flip the sprite
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            isFacingRight = value;
+        }
+    }
 
     private void Awake()
     {
         //gets the rigidbody.
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>(); //gets the animator
     }
 
     // Start is called before the first frame update
@@ -47,7 +83,22 @@ public class PlayerController : MonoBehaviour
         _moveInput = context.ReadValue<Vector2>();
         //checks if the player is moving
         IsMoving = _moveInput != Vector2.zero;
+        //sets the facing direction
+        SetFacingDirection(_moveInput);
     }
-    
+
+    //sets the sprite direction
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
     
 }
