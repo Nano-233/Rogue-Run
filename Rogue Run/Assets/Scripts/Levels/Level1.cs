5cd093be43acf088821c07510979618afa812f02
Nano <86750728+Nano-233@users.noreply.github.com>
2024-07-16 23:29:03 +0800

    Applied new randomizer to level 1

--- Rogue Run/Assets/Scripts/Levels/Level1.cs
+++ Rogue Run/Assets/Scripts/Levels/Level1.cs
@@ -1,45 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Level1 : MonoBehaviour
 {
     //parent
     public GameObject enemies;
-    
-    //prefabs to be instantiated
-    public GameObject octo;
-    public int octoNum = 6;
 
-    private Vector2[] _octoPos = new[]
-    {
-        new Vector2(-49f, 3.5f), new Vector2(-45f, 3.5f),
-        new Vector2(-11f, -8f), new Vector2(-25f, -8f), new Vector2(-26f, -3f),
-        new Vector2(-20f, 1f)
-    };
-    
-    
+    //setups for ground enemies
+    public GameObject octo;
+    public int octoCount = 3;
+
+    public int groundCount = 3;
+    private int _groundTypes = 1;
+
+
+    //array of positions to spawn ground enemies in
+    [SerializeField] private List<Transform> groundPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        //spawn octos
-        List<int> availableSpawns = new List<int>();
-        int number;
-        for (int i = 0; i < octoNum; i++)
+        //randomizes the spawns by taking random positions
+        Transform[] groundTransforms = groundPos.OrderBy(x => Random.value).Take(groundCount).ToArray();
+
+
+        int[] groundCounts = new[] { octoCount };
+        GameObject[] groundObj = new[] { octo };
+        int spawnCount = 0;
+        //spawn all of the required ground enemies randomly
+        for (int pointer = 0; pointer < _groundTypes; pointer++)
         {
-            do
+            for (int i = 0; i < groundCounts[pointer]; i++)
             {
-                number = Random.Range(0, _octoPos.Length);
-            } while (availableSpawns.Contains(number));
-            
-            availableSpawns.Add(number);
-            Instantiate(octo, _octoPos[number], Quaternion.identity, enemies.transform);
+                Instantiate(groundObj[pointer], groundTransforms[spawnCount].position, Quaternion.identity,
+                    enemies.transform);
+                spawnCount++;
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
